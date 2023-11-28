07c87d4b6b69f2593546701b4c20c281c29a7b87
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-11-28 16:17:05 +0100

    update: update app component to use react query and chakra providers and the widget
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+
+import WidgetProvider from "./components/WidgetProvider";
+
+import { theme } from "../styles/theme";
+
 function App() {
-    return <>The beginning</>;
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            }),
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <AnimatePresence mode="wait">
+                    <WidgetProvider />
+                </AnimatePresence>
+            </ChakraProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
