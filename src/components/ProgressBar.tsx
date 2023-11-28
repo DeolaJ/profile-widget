dd32f2be400396ba0347f0b18e94fd3bcc5c24e6
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-11-28 16:05:47 +0100

    feat: create Progress bar and Header
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,40 @@
+import { Progress, ProgressLabel } from "@chakra-ui/react";
+
+import { calculateProgress, calculateTotal } from "../utils/helpers";
+
+import { TaskGroupType } from "../types";
+
+export default function ProgressBar({
+    taskGroups,
+    localTaskGroups,
+}: {
+    taskGroups: TaskGroupType[];
+    localTaskGroups: TaskGroupType[];
+}) {
+    const taskTotal = calculateTotal(taskGroups);
+    const localTaskTotal = calculateTotal(localTaskGroups, true);
+    const progressPercent = calculateProgress(localTaskTotal, taskTotal);
+
+    return (
+        <Progress
+            value={progressPercent}
+            h="20px"
+            borderRadius="16"
+            colorScheme="primary.success"
+            bgColor="primary.success.300"
+        >
+            <ProgressLabel
+                width={`${progressPercent > 6 ? progressPercent : 6}%`} // Leave enough room for the label to be rendered horizonatally
+                left="0"
+                transform="translate(0%, -50%)"
+                textAlign="right"
+                pr="4"
+                fontSize="md"
+                fontWeight={600}
+                color="primary.success.300"
+            >
+                {progressPercent}%
+            </ProgressLabel>
+        </Progress>
+    );
+}
