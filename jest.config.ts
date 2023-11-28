2e6b8af64f4c8712eb9dddd08ed13d2392f1e7b4
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-11-28 19:09:37 +0100

    feat: set up jest and react testing library
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    moduleDirectories: ["node_modules"],
+    moduleNameMapper: {
+        "^.+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "jest-transform-stub",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
